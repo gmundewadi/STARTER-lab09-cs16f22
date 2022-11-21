f608b76f2b4ab5304375443a9f0a5bc0fb5a6b57
Gautam Mundewadi <gautammundewadi@ucsb.edu>
2022-11-21 11:51:35 -0800

    gm - parse data from csv

--- main.cpp
+++ main.cpp
@@ -1,23 +1,48 @@
 #include "matplotlibcpp.h"
+
+#include <fstream>
+#include <sstream>
+
+#include <string>
 #include <vector>
-namespace plt = matplotlibcpp;
+
 using namespace std;
 
+vector<vector<double>> parseData(string pathToFile) {
+    vector<vector<double>> result;
 
-vector<vector<double>> parse(string filename) {
-    vector<vector<double>> data;
-    //import data from file
+    ifstream ifs(pathToFile);
+    if(ifs.fail()){ 
+        cerr << "Could not open file " + pathToFile << endl;
+        exit(2);
+    }
 
-    return data;
+    string line;
+    int count = 0;
+    getline(ifs,line);
+    while (!ifs.eof()) {  
+        // parse each line into (double, double, double) format  
+        string num;
+        vector<double> nums;    
+        istringstream ss(line);
+        while (getline(ss, num, ',')) {
+            if(num != ""){
+                nums.push_back(stod(num));
+            } 
+        }
+        // of add if no NULL numbers
+        if(nums.size() == 3){ 
+            result.push_back(nums);
+        } 
+        getline(ifs,line);
+    }    
+
+    ifs.close();
+    return result;
+    
 }
 
-// void plot(vector<vector<double>> data) {
-//     //plot data
-// }
-
 int main() {
-    vector<int> datas = {1, 3};
-    plt::plot(datas);
-    plt::save("test.png");
+    vector<vector<double>> data = parseData("./2008CompilationData.csv");
 }
 
